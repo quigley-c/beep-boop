f165666e6f6f334feacd45f9450706ab814a34ab
quigley-c <quigleyc@allegheny.edu>
2020-05-04 05:46:27 +0000

    movement, animations

--- src/main.lua
+++ src/main.lua
@@ -9,29 +9,34 @@ function love.load()
 
 	love.window.setFullscreen(false)
 
-	camera = {x, y, zoom}
-	player = { x, y, spd, img}
+	camera = {x, y, zoom, rotation}
+	player = {x, y, spd, img, wants_left, wants_right, is_walking, anim}
 
 	camera.x = camera.x or 0
 	camera.y = camera.y or 0
-	camera.zoom = camera.zoom or 0.5
+	camera.zoom = camera.zoom or 1
+	camera.rotation = 0
 
 	player.x = player.x or 150
 	player.y = player.y or 150
-	player.spd = player.speed or 150
+	player.spd = player.speed or 60
+	player.wants_left = player.wants_left or false
+	player.wants_right = player.wants_right or false
 
-	image = love.graphics.newImage("player.png")
-	animation = new_animation(love.graphics.newImage("player.png"), 32, 32, 1)
+	spr_walk_left = new_animation(love.graphics.newImage("walk-left.png"), 32, 32, 1)
+	spr_walk_right = new_animation(love.graphics.newImage("walk-right.png"), 32, 32, 1)
+	spr_left = new_animation(love.graphics.newImage("left.png"), 32, 32, 1)
+	spr_right = new_animation(love.graphics.newImage("right.png"), 32, 32, 1)
 
-	love.graphics.setNewFont(12)
+	player.anim = player.anim or spr_right
 end
 
 function new_animation(img, framew, frameh, dur)
 	local animation = {}
-	animation.spriteSheet = image
+	animation.spriteSheet = img
 	animation.quads = {}
 
-	for y = 0, image:getHeight() - frameh, frameh do
+	for y = 0, img:getHeight() - frameh, frameh do
 		for x = 0, img:getWidth() - framew, framew do
 			table.insert(animation.quads, love.graphics.newQuad(x, y, framew, frameh, img:getDimensions()))
 		end
@@ -44,22 +49,46 @@ function new_animation(img, framew, frameh, dur)
 end
 
 function love.update(dt)
+
 	if love.keyboard.isDown("left") then
 		player.x = player.x - player.spd * dt
+		if not love.keyboard.isDown('right') then
+			player.wants_left = true
+			player.wants_right = false
+		end
+		player.is_walking = true
 	end
 	if love.keyboard.isDown("up") then
 		player.y = player.y - player.spd * dt
+		player.is_walking = true
 	end
 	if love.keyboard.isDown("down") then
 		player.y = player.y + player.spd * dt
+		player.is_walking = true
 	end
 	if love.keyboard.isDown("right") then
 		player.x = player.x + player.spd * dt
+		if not love.keyboard.isDown('left') then
+			player.wants_right = true
+			player.wants_left = false
+		end
+		player.is_walking = true
 	end
 
-	animation.currentTime = animation.currentTime + dt
-	if animation.currentTime >= animation.duration then
-		animation.currentTime = animation.currentTime - animation.duration
+	if not love.keyboard.isDown('left') and
+	not love.keyboard.isDown('right') and
+	not love.keyboard.isDown('down') and
+	not love.keyboard.isDown('up') then
+		player.is_walking = false
+	end
+	
+	if love.keyboard.isDown('left') and love.keyboard.isDown('right') then
+		player.is_walking = false
+	end
+
+	player.anim.currentTime = player.anim.currentTime + dt
+	if player.anim.currentTime >= player.anim.duration then
+		player.anim.currentTime = player.anim.currentTime - player.anim.duration
 	end
 end
 
@@ -67,14 +96,51 @@ function love.keypressed(key)
 	if key == 'escape' then
 		love.event.quit()
 	end
+
+	if key == 'left' then
+	end
+	
+	if key == 'right' then
+	end
 end
 
+function love.keyreleased(key)
+end
+
+function camera_set()
+	love.graphics.push()
+	love.graphics.rotate(camera.rotation)
+	love.graphics.scale(1 / camera.zoom, 1 / camera.zoom)
+	love.graphics.translate(camera.x, camera.y)
+end
+
+function camera_unset()
+	love.graphics.pop()
+end
 
 function love.draw()
 
-	love.graphics.scale(1 / camera.zoom, 1 / camera.zoom)
+	camera_set()
 
-	local cur_frame = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
-	love.graphics.draw(animation.spriteSheet, animation.quads[cur_frame],  player.x, player.y)
+	if player.wants_left then
+		if player.is_walking then
+			player.anim = spr_walk_left
+		else
+			player.anim = spr_left
+		end
+	elseif player.wants_right then
+		if player.is_walking then
+			player.anim = spr_walk_right
+		else
+			player.anim = spr_right
+		end
+	end
 
+	love.graphics.print(
+	"player wants left:" .. tostring(player.wants_left) ..
+	"\tplayer wants right:" .. tostring(player.wants_right), 50, 5)
+
+	local cur_frame = math.floor(player.anim.currentTime / player.anim.duration * #player.anim.quads) + 1
+	love.graphics.draw(player.anim.spriteSheet, player.anim.quads[cur_frame],  player.x, player.y)
+	camera_unset()
 end
